79abb5038140bb1d6add41a2d963f6611f198d87
Jace <jacemcgough1@gmail.com>
2018-08-08 23:07:05 -0600

    Colors + Fonts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home-text'>
-      <h3>Hello</h3>
-      <p>My name is Jace McGough.</p>
-      <p>I think Javascript is cool.</p>
+      <h3 className='home-header'>Hello</h3>
+      <p className='home-description'>
+        My name is Jace McGough.
+        <br />
+        I think Javascript is cool.
+      </p>
     </div>
   )
 }
