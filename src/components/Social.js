607689a63383fae53460655437a92e1194a60cf1
Jace <jacemcgough1@gmail.com>
2018-08-09 22:12:21 -0600

    Better security

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,16 +8,16 @@ const Social = () => {
 
   return (
     <div className='social-container'>
-    <a href='mailto:jacemcgough1@gmail.com' target='_blank' >
+    <a href='mailto:jacemcgough1@gmail.com' target='_blank' rel='noopener noreferrer'>
       <img src={Email} alt='Email' height='50px' />
     </a>
-    <a href='https://github.com/TangyOhio' target='_blank' >
+    <a href='https://github.com/TangyOhio' target='_blank' rel='noopener noreferrer'>
       <img src={GitHub} alt='GitHub' height='50px' />
     </a>
-    <a href='https://www.linkedin.com/in/jacemcgough/' target='_blank' >
+    <a href='https://www.linkedin.com/in/jacemcgough/' target='_blank' rel='noopener noreferrer'>
       <img src={LinkedIn} alt='LinkedIn' height='50px' />
     </a>
-    <a href='https://twitter.com/TangyOhio' target='_blank' >
+    <a href='https://twitter.com/TangyOhio' target='_blank' rel='noopener noreferrer'>
       <img src={Twitter} alt='Twitter' height='50px' />
     </a>
     </div>
