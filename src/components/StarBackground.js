c309e77d9896f79a3f23f2a07e81f2c1355ffd2f
Jace <jacemcgough1@gmail.com>
2018-08-10 23:07:57 -0600

    Start of stars + cleaning

--- src/components/StarBackground.js
+++ src/components/StarBackground.js
@@ -6,9 +6,7 @@ class StarBackground extends React.Component {
     const { children } = this.props
     return (
       <div className='star-bg'>
-        <div id='stars1' />
-        <div id='stars2' />
-        <div id='stars3' />
+        {/* <span id='stars' /> */}
         {children}
       </div>
     )
