99dc06543c79eb3083dc7e9ddf1d9be45bdce962
Jace <jacemcgough1@gmail.com>
2018-08-15 13:55:10 -0600

    Editing

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div className='about-description'>
         <p>I'm a Junior Web Developer, and I strive to be a Full Stack Developer someday.</p>
         <br />
-        <p>I enjoy using React in the front and Ruby on Rails in the back. I'm learning SASS, which is what I built this site with.</p>
+        <p>I enjoy using React in the front and Ruby on Rails in the back. I'm currently learning SASS, which is what I built this site with.</p>
         <br />
         <p>I love computers, and I've been playing with them ever since I was a kid. I enjoy video games, spending time with my family, and giant robot fights.</p>
       </div>
